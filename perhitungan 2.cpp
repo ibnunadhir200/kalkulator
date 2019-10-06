e46f39c781f97c0253b4129e085ad9f5d0943bce
ibnunadhir200 <56227065+ibnunadhir200@users.noreply.github.com>
2019-10-06 21:47:12 +0700

    Add files via upload

---
+++ perhitungan 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,c,d,e,f;
+	
+	cout << "Masukkan Nilai A: ";
+	cin >> a;
+	cout << "Masukkan Nilai B: ";
+	cin >> b;
+	c = a+b;
+	d = a-b;
+	e = a*b;
+	f = a/b;
+	
+	cout << "+++++ HASIL +++++ " << endl;
+	cout << "Hasil Penjumlahan = " <<c<<endl;
+	cout << "Hasil Pengurangan = " <<d<<endl;
+	cout << "Hasil Perkalian =   " <<e<<endl;
+	cout << "Hasil Pembagian =   " <<f<<endl;
+	
+	cout<< "by ibnu nadhir" ;
+	return 0;
+}
